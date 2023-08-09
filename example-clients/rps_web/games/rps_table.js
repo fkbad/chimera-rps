aae9a701e98641d2c2a12c49f4adabdda6ab4fd8
fkbad <fkb@uchicago.edu>
2023-08-09 13:43:35 -0500

    created stylesheet and added a group of logs to store the game actions performed and any error done. currently aligning using block display, but I'm now going to try flex display

--- example-clients/rps_web/games/rps_table.js
+++ example-clients/rps_web/games/rps_table.js
@@ -5,59 +5,71 @@
 
 
 function createTable(table) {
-    /*
-     * function to actually create the HTML for the table
-     *
-     * Inputs:
-     *    table: the table class div from the rps html file
-     *    game_id: the game_id variant to play, taken from URLParams
-     */
+  /*
+   * function to actually create the HTML for the table
+   *
+   * Inputs:
+   *    table: the table class div from the rps html file
+   *    game_id: the game_id variant to play, taken from URLParams
+   */
 
-    // first get the game_id from the URL
-    const url_params = new URLSearchParams(window.location.search)
+  console.log("creating table...")
+  // first get the game_id from the URL
+  const url_params = new URLSearchParams(window.location.search)
 
-    // search params uses dict-like get syntax
-    // has separate method to check whether or not a field is in the URL
-    const game_id = url_params.get("game_id");
+  // search params uses dict-like get syntax
+  // has separate method to check whether or not a field is in the URL
+  const game_id = url_params.get("game_id");
 
-    if (!game_id) {
-        table.innerText = "No subgame chosen";
-      return
-    };
+  if (!game_id) {
+      table.innerText = "No subgame chosen";
+    return
+  };
 
-    // Clear the table div
-    table.innerHTML = "";
+  // Clear the table div
+  table.innerHTML = "";
 
-    // Declare an array of words for the buttons using let
-    const moves = ["rock","paper","scissors","lizard","spock"]
+  let fieldset = document.createElement("fieldset");
+  let legend = document.createElement("legend");
+  let buttons = document.createElement("div");
 
-    // Check the game_id and create the buttons accordingly
-    if (game_id == "rps") {
-        // Create three buttons using let
-        for (let i = 0; i < 3; i++) {
-            // Create a button element using const
-            const button = document.createElement("button");
-            // Set the button text to the word at index i
-            button.innerText = moves[i];
-      //
-            // Append the button to the table div
-            table.appendChild(button);
-        }
-    } else if (game_id == "rpsls") {
-        // Create five buttons using let
-        for (let i = 0; i < 5; i++) {
-            // Create a button element using const
-            const button = document.createElement("button");
-            // Set the button text to the word at index i
-            button.innerText = moves[i];
-            // Append the button to the table div
-            table.appendChild(button);
-        }
-    } else {
-        // Display an error message
-        table.innerText = "Invalid game chosen";
+  fieldset.appendChild(legend)
+
+  // Declare an array of words for the buttons using let
+  const moves = ["rock","paper","scissors","lizard","spock"]
+
+  // Check the game_id and create the buttons accordingly
+  if (game_id == "rps") {
+    for (let i = 0; i < 3; i++) {
+        const button = document.createElement("button");
+        // Set the button text to a move
+        button.innerText = moves[i];
+        // Append the button to the table div
+        buttons.appendChild(button);
     }
+  } else if (game_id == "rpsls") {
+    // Create five buttons using let
+    for (let i = 0; i < 5; i++) {
+      // Create a button element using const
+      const button = document.createElement("button");
+      // Set the button text to the word at index i
+      button.innerText = moves[i];
+      // Append the button to the table div
+      buttons.appendChild(button);
+    }
+  } else {
+    // Display an error message
+    table.innerText = "Invalid game chosen";
+  }
+  
+  // after the buttons have been created, add them to the fieldset
+  fieldset.appendChild(buttons)
+
+  // after the fieldset has been populated, add it to the table
+  table.appendChild(fieldset)
 }
 
 // Export the createTable function using a named export
 export { createTable };
+
+
